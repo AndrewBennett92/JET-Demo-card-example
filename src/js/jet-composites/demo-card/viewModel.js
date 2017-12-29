ad47d61e35a4b5e4be9a2093a536bfe9f7844831
Andrew Bennett <andybennett92@hotmail.com>
2017-12-29 17:51:36 +0000

    component files done

--- src/js/jet-composites/demo-card/viewModel.js
+++ src/js/jet-composites/demo-card/viewModel.js
@@ -4,35 +4,45 @@
 */
 define(
     ['ojs/ojcore', 'knockout', 'jquery'], function (oj, ko, $) {
-    'use strict';
-    
-    function ExampleComponentModel(context) {
-        var self = this;
-        self.composite = context.element;
-        //Example observable
-        self.messageText = ko.observable('Hello from Example Component');
+        'use strict';
 
-        context.props.then(function (propertyMap) {
-            //Store a reference to the properties for any later use
-            self.properties = propertyMap;
+        function ExampleComponentModel(context) {
+            var self = this;
+            self.initials = null;
+            self.workFormatted = null;
+            var element = context.element;
 
-            //Parse your component properties here 
+            /**
+              * Formats a 10 digit number as a phone number.
+              * @param  {number} number The number to format
+              * @return {number}        The formatted phone number
+              */
+            var formatPhoneNumber = function (number) {
+                return number.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+            }
 
-        });
-    };
-    
-    //Lifecycle methods - uncomment and implement if necessary 
-    //ExampleComponentModel.prototype.activated = function(context){
-    //};
+            // The props field on context is a Promise. Once that resolves,
+            // we can access the properties that were defined in the composite metadata
+            // and were initially set on the composite DOM element.
+            context.props.then(function (properties) {
+                if (properties.name) {
+                    var initials = properties.name.match(/\b\w/g);
+                    self.initials = (initials.shift() + initials.pop()).toUpperCase();
+                }
+                if (properties.workNumber)
+                    self.workFormatted = formatPhoneNumber(properties.workNumber);
+            });
 
-    //ExampleComponentModel.prototype.attached = function(context){
-    //};
-
-    //ExampleComponentModel.prototype.bindingsApplied = function(context){
-    //};
-
-    //ExampleComponentModel.prototype.detached = function(context){
-    //};
-
-    return ExampleComponentModel;
-});
+            /**
+              * Flips a card
+              * @param  {MouseEvent} event The click event
+              */
+            self.flipCard = function (model, event) {
+                if (event.type === 'click' || (event.type === 'keypress' && event.keyCode === 13)) {
+                    $(element.childNodes[0]).toggleClass('flipped');
+                }
+            };
+        };
+        
+        return ExampleComponentModel;
+    });
